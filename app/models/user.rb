1d642973ad89e9230ebe4d53822e7d7a20378975
Daniel Polo <od.polo@gmail.com>
2021-03-19 03:08:54 -0600

    feat: Travix config

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    return '' if !first_name && !last_name
+
+    return "#{first_name.capitalize} #{last_name.capitalize}" if first_name && last_name
+
+    return first_name.capitalize if first_name
+
+    return last_name.capitalize if last_name
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#full_name returns a string with the complete name" do
+    user = User.new(
+      email: 'od.polo@gmail.com',
+      first_name: 'Daniel',
+      last_name: 'Polo',
+      password: '123456'
+    )
+    full_name = user.full_name
+    assert_equal(full_name, 'Daniel Polo')
+  end
+
+  test "#full_name works when no last_name" do
+    user = User.new(
+      email: 'od.polo1@gmail.com',
+      first_name: 'daniel',
+      password: '123456'
+    )
+    full_name = user.full_name
+    assert_equal(full_name, 'Daniel')
+  end
+
+  test "#full_name works when no last_name nor first_name" do
+    user = User.new(
+      email: 'od.polo1@gmail.com',
+      password: '123456'
+    )
+    full_name = user.full_name
+    assert_equal(full_name, '')
+  end
 end
+
+
